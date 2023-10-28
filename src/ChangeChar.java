f5879f44d11195353b9cdcf1c7ce9fa9b5e03f25
Hopeman-ever <67267275+Hopeman-ever@users.noreply.github.com>
2023-10-28 16:19:16 +0800

    Update ChangeChar.java

--- src/ChangeChar.java
+++ src/ChangeChar.java
@@ -7,5 +7,7 @@ public class ChangeChar {
         System.out.println("北京\n天津\n上海");
         System.out.println("北京\\天津\\上海");
         System.out.println("北京\r天津\r上海");
+
+        int c = 112121;
     }
 }
